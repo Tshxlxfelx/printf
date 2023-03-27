38dcfc12b0219169d3f79992580d1cd1877cb0f4
Tshxlxfelx <tsholofelochoma@outlook.com>
2023-03-26 21:55:09 -0700

    updated 2

--- _printf.c
+++ _printf.c
@@ -65,21 +65,3 @@ write(1, &buffer[0], *buff_ind);
 *buff_ind = 0;
 }
 
-vi putchar
-
-#include <unistd.h>
-#include "main.h"
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
